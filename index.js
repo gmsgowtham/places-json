f915666c53b62ee520dca4f0cab8145d0a13e112
gowthamcodingmart <gowtham@codingmart.com>
2017-11-29 15:36:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,3 @@
-// const Pbf = require('pbf')
-// const fs = require('fs')
-// const path = require('path')
-
-// var pbf = new Pbf(fs.readFileSync(path.join(__dirname, 'cities.pbf')))
-// var cities = []
-
-// var lastLat = 0
-// var lastLon = 0
-
-// while (pbf.pos < pbf.length) {
-//     cities.push(pbf.readMessage(readCity, {
-//         name: '',
-//         country: ''
-//     }))
-// }
-
-// let hash = JSON.stringify(cities);
-// fs.writeFile("./cities.txt", hash, function(err) {
-//     if(err) {
-//         return console.log(err);
-//     }
-
-//     console.log("The file was saved!");
-// }); 
-// // var blob = blobUtil.createBlob([hash], {type: "text/plain;charset=utf-8"});
-// // FileSaver.saveAs(blob, "cities.txt");
-
-// module.exports = cities
-
-// function readCity(tag, city, pbf) {
-//     if (tag === 1) city.name = pbf.readString()
-//     else if (tag === 2) city.country = pbf.readString()
-//     else if (tag === 3) city.altCountry = pbf.readString()
-// }
-
 const cities = require('./cities.json');
 const regions = require('./region-country-map.json');
 let regionCountryMap = Object.assign({}, regions);
